51c7786b84a58df5948c6cf92ca2922f3536319a
liaosiwei <liaosiwei@baidu.com>
2015-12-04 17:32:31 +0800

    finish break program

--- break.go
+++ break.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 	"math"
+	"os/exec"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 
@@ -33,23 +35,60 @@ func Mean(data []float64) float64 {
 }
 
 func Ping(host string, repeat int) (data []float64) {
+	if repeat < 1 {
+		panic("ping time cannot less than 1")
+	}
 	for i := 0; i < repeat; i++ {
 		time.Sleep(1 * time.Second)
 
-		dur, err := ping.Pinger(host, 3)
+		dur, err := ping.Pinger(host, 1)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			dur = 10000.0 // if ping fails, the ping time sets to 10s
 		}
-		fmt.Println("ping ", host, ": ", dur)
+		log.Println("ping ", host, ": ", dur)
 		data = append(data, dur)
 	}
 	return
 }
 
+func SetProxy(proxy string) error {
+	out, err := exec.Command("networksetup", "-getautoproxyurl", "Wi-Fi").Output()
+	if err != nil {
+		panic(err)
+	}
+	pacPair := strings.Split(string(out), "Enable")
+
+	if len(pacPair) != 2 {
+		panic(`command networksetup -getautoproxyurl "Wi-Fi" is not correct`)
+	}
+
+	pacAddress := pacPair[0]
+	start := strings.Index(pacAddress, "http")
+	var pac string
+	if start != -1 {
+		pac = strings.TrimSpace(pacAddress[strings.Index(pacAddress, "http"):])
+		if proxy == pac {
+			return nil
+		}
+	}
+
+	out, err = exec.Command("networksetup", "-setautoproxyurl", "Wi-Fi", proxy).Output()
+	if err != nil {
+		log.Fatal("switching pac from " + pac + " to " + proxy + " failed: " + string(out))
+	}
+	log.Println("switching pac from " + pac + " to " + proxy)
+	return err
+}
+
 type Config struct {
-	Nodes  []string
+	Nodes  map[string]Host
 	Repeat int
+}
+
+type Host struct {
+	Address   string
+	PingValue float64
 }
 
 var configFile string
@@ -58,29 +97,44 @@ func main() {
 	flag.StringVar(&configFile, "config", "config.toml", "the config file location")
 	flag.Parse()
 	var config Config
-	nodeInfo := make(map[string]float64)
 
 	if _, err := toml.DecodeFile(configFile, &config); err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	var wg sync.WaitGroup
-	for _, node := range config.Nodes {
-		wg.Add(1)
-		go func(url string) {
-			defer wg.Done()
-			mean := Mean(Ping(url, config.Repeat))
-			nodeInfo[url] = mean
-		}(node)
+	if config.Repeat < 1 {
+		panic("ping time cannot less than 1")
 	}
-	wg.Wait()
-	minValue := 1000000.0
-	var minNode string
-	for k, v := range nodeInfo {
-		if v < minValue {
-			minValue = v
-			minNode = k
+
+	var Do = func() {
+		var wg sync.WaitGroup
+		for node, _ := range config.Nodes {
+			wg.Add(1)
+			go func(url string) {
+				defer wg.Done()
+				mean := Mean(Ping(url, config.Repeat))
+				host := config.Nodes[url]
+				host.PingValue = mean
+				config.Nodes[url] = host
+			}(node)
+		}
+		wg.Wait()
+		minValue := 1000000.0
+		var minNode string
+		for k, v := range config.Nodes {
+			if v.PingValue < minValue {
+				minValue = v.PingValue
+				minNode = k
+			}
+		}
+		log.Println("choose ", minNode, " with min ping ", minValue)
+		if err := SetProxy(config.Nodes[minNode].Address); err != nil {
+			log.Fatal(err)
 		}
 	}
-	fmt.Println(minNode, " with min ping ", minValue)
+	for {
+		go Do()
+		select {
+		case <-time.After(time.Duration(2) * time.Minute):
+		}
+	}
 }
